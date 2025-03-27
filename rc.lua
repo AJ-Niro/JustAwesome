36d5ea631ea86fbba2ae384b47c4e00792729236
AJ-Niro <andresninorodil@gmail.com>
2025-03-26 21:15:25 -0400

    refactor: extract the rules from rc.lua to it own module

--- rc.lua
+++ rc.lua
@@ -4,7 +4,7 @@ pcall(require, 'luarocks.loader')
 
 -- JustAwesome core modules
 local keybindings = require('core.keybindings')
-local mousebindings = require('core.mousebindings')
+local rules = require('core.rules')
 
 -- Standard awesome library
 local awful = require('awful')
@@ -281,64 +281,7 @@ root.keys(keybindings.global)
 
 -- {{{ Rules
 -- Rules to apply to new clients (through the "manage" signal).
-awful.rules.rules = {
-  -- All clients will match this rule.
-  {
-    rule = {},
-    properties = {
-      border_width = beautiful.border_width,
-      border_color = beautiful.border_normal,
-      focus = awful.client.focus.filter,
-      raise = true,
-      keys = keybindings.client,
-      buttons = mousebindings,
-      screen = awful.screen.preferred,
-      placement = awful.placement.no_overlap + awful.placement.no_offscreen,
-    },
-  },
-
-  -- Floating clients.
-  {
-    rule_any = {
-      instance = {
-        'DTA', -- Firefox addon DownThemAll.
-        'copyq', -- Includes session name in class.
-        'pinentry',
-      },
-      class = {
-        'Arandr',
-        'Blueman-manager',
-        'Gpick',
-        'Kruler',
-        'MessageWin', -- kalarm.
-        'Sxiv',
-        'Tor Browser', -- Needs a fixed window size to avoid fingerprinting by screen size.
-        'Wpa_gui',
-        'veromix',
-        'xtightvncviewer',
-      },
-
-      -- Note that the name property shown in xprop might be set slightly after creation of the client
-      -- and the name shown there might not match defined rules here.
-      name = {
-        'Event Tester', -- xev.
-      },
-      role = {
-        'AlarmWindow', -- Thunderbird's calendar.
-        'ConfigManager', -- Thunderbird's about:config.
-        'pop-up', -- e.g. Google Chrome's (detached) Developer Tools.
-      },
-    },
-    properties = { floating = true },
-  },
-
-  -- Add titlebars to normal clients and dialogs
-  { rule_any = { type = { 'normal', 'dialog' } }, properties = { titlebars_enabled = true } },
-
-  -- Set Firefox to always map on the tag named "2" on screen 1.
-  -- { rule = { class = "Firefox" },
-  --   properties = { screen = 1, tag = "2" } },
-}
+awful.rules.rules = rules.generate()
 -- }}}
 
 -- {{{ Signals
