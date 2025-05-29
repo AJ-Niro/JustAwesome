b05bbed6e5e386d6415bb32c6f022bf6d4379fc2
AJ-Niro <andresninorodil@gmail.com>
2025-05-28 21:16:32 -0400

    feat(apps): run picom autostart

--- rc.lua
+++ rc.lua
@@ -151,3 +151,6 @@ client.connect_signal('unfocus', function(c)
   c.border_color = beautiful.border_normal
 end)
 -- }}}
+
+local current_path = gears.filesystem.get_configuration_dir()
+awful.spawn.with_shell('picom -b --config ' .. current_path .. 'apps/picom.conf')
